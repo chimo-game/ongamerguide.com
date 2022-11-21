a0a51545be4eccae6483ab01ffd0c2179e3e3fb6
tolapao <62795957+XANuN@users.noreply.github.com>
2022-11-22 00:52:10 +0700

    Update random-number-generator2.js

--- assets/js/random-number-generator2.js
+++ assets/js/random-number-generator2.js
@@ -12,7 +12,7 @@ function randWithVariation2(min, max, variation) {
     return r2;
 }
 
-var rand = randWithVariation2(10, 100, 10);
+var rand = randWithVariation2(10, 00, 10);
 document.getElementById('rnd2').innerHTML = rand();
 setInterval(() => {
     document.getElementById('rnd2').innerHTML = rand();
