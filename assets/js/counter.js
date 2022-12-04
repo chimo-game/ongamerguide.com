e060cad15b4ad369b73a280e453bc71e7ea19738
tolapao <62795957+XANuN@users.noreply.github.com>
2022-12-04 23:03:50 +0700

    Update counter.js

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -6,13 +6,13 @@ function startCount() {
 }
 
 function count() {
-    var rand_no = Math.ceil(4 * Math.random()); // 9 = random decrement amount. Counter will decrease anywhere from 1 - 9.
+    var rand_no = Math.floor(4 * Math.random()); // 9 = random decrement amount. Counter will decrease anywhere from 1 - 9.
     var el = document.getElementById('counter');
     var currentNumber = parseFloat(el.innerHTML);
     var newNumber = currentNumber + rand_no;
     if (newNumber > 0) {
         el.innerHTML = newNumber;
     } else {
-        el.innerHTML = "Sold Out"; // This message is displayed when the counter reaches zero.
+        el.innerHTML = "Bingo"; // This message is displayed when the counter reaches zero.
     }
 }
